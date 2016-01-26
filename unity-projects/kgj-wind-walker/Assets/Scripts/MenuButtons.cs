03a10c52db720220808bc511e645046f89c8deda
SzymonR <s.rekawek@kainos.com>
2016-01-26 22:25:24 +0100

    menu ui

---
+++ unity-projects/kgj-wind-walker/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	void Update() {
+		QuitOnEscape ();
+	}
+
+	public void StartGame() {
+		Application.LoadLevel ("GameScene");
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+
+	private void QuitOnEscape() {
+		if (Input.GetKey ("escape")) {
+			ExitGame();
+		}
+	}
+}
