9dc4c9f02880c0b3a5a4684fed8212162ad8f1df
SzymonR <s.rekawek@kainos.com>
2016-01-29 21:53:57 +0100

    tree scene moving and rain spot spawn

---
+++ unity-projects/kgj-wind-walker/Assets/Scripts/RainPointerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainPointerController : MonoBehaviour {
+
+	public float branchLeftBorder = 0;
+	public float branchRightBorder = 0;
+	public float groundHeight = 0;
+	public GameObject rainSpot;
+	public GameObject runney;
+
+	void Start () {
+		wave ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void wave() {
+		StartCoroutine(SpawnRainSpots(3f, 3f, 10));
+
+	}
+
+	IEnumerator SpawnRainSpots(float startWait, float interval, int numberOfSpots) {
+		yield return new WaitForSeconds (startWait);
+		for (int i = 0; i < numberOfSpots; i++) {
+			Instantiate (rainSpot, CalculateRainSpotPosition (), Quaternion.identity);
+			yield return new WaitForSeconds (interval);
+		}
+
+
+	}
+
+	private Vector3 CalculateRainSpotPosition (){
+		float calcPositionX = Random.Range(branchLeftBorder, branchRightBorder);
+
+		return new Vector3 (calcPositionX, groundHeight, 0);
+	}
+}
