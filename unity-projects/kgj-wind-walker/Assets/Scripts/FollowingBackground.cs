e671938d5ccac4725457ba2078f5ffebd1a064b3
SzymonR <s.rekawek@kainos.com>
2016-01-30 18:16:02 +0100

    scrolling lukasz

---
+++ unity-projects/kgj-wind-walker/Assets/Scripts/FollowingBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingBackground : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = gameObject.transform.position;
+		temp.x = player.transform.position.x;
+		gameObject.transform.position = temp;
+	
+	}
+}
