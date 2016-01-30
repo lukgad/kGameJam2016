151a0b517050862c27c04522272667e61be38247
SzymonR <s.rekawek@kainos.com>
2016-01-30 11:41:43 +0100

    better rainspots

---
+++ unity-projects/kgj-wind-walker/Assets/Scripts/RainSpot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainSpot : MonoBehaviour {
+
+	public float requiredWateringTime = 1f;
+
+	private static readonly string PLAYER_TAG ="Player";
+	private float spacePressTime =  0f;
+	private float spaceReleaseTime =  0f;
+	private bool spacePressed = false;
+
+	
+	void Update () {
+		appendSpaceTime ();
+		handleWateringTime ();
+	}
+
+	void OnTriggerStay2D(Collider2D collider) {
+		if(collider.gameObject.tag == PLAYER_TAG) {
+			getSpace ();
+		}
+	}
+
+	private void getSpace() {
+		if(Input.GetKeyDown("space")) {
+			spacePressed = true;	
+		}
+		if(Input.GetKeyUp("space")) {
+			spacePressed = false;
+		}
+	}
+
+	private void appendSpaceTime() {
+		if (spacePressed) {
+			spacePressTime += Time.deltaTime;
+		}
+	}
+
+	private void handleWateringTime() {
+		if (spacePressTime > requiredWateringTime) {
+			Debug.Log ("Watering time: " + spacePressTime + "\n Destroying spot");
+			Destroy (gameObject);
+		}
+	}
+}
