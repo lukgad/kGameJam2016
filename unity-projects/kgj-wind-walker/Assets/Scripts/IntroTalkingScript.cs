1c529ecf7a07909156861ef358cc0b182bee9b2a
SzymonR <s.rekawek@kainos.com>
2016-01-30 17:29:25 +0100

    ritual animations applied

---
+++ unity-projects/kgj-wind-walker/Assets/Scripts/IntroTalkingScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroTalkingScript : MonoBehaviour {
+	public GameObject left;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void go() {
+		//StartCoroutine(SpawnRainSpots(
+	}
+
+	/*
+	IEnumerator SpawnRainSpots(GameObject cloud, float startWait) {
+		yield return new WaitForSeconds (startWait);
+			cloud.GetComponent<Renderer>().sortingLayerName = "UI";
+			Instantiate (cloud, cloud.transform.position, Quaternion.identity);
+		}
+	}*/
+}
